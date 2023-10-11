41aa4d79d34e90d0da2626d66912ec65d22b1822
captap <hanapsk61@gmail.com>
2023-10-12 02:51:11 +0900

    Added comments
    Added comments

--- src/todo.js
+++ src/todo.js
@@ -8,6 +8,7 @@ const FINISHED = "FINISHED";
 
 let pendingTasks, finishedTasks; 
 
+// 사용자가 입력한 텍스트를 받아와서 할 일 항목을 생성하고 고유한 ID를 부여
 function getTaskObject(text) {
     console.log(text);
     return {
@@ -16,42 +17,49 @@ function getTaskObject(text) {
     };
 }
 
+// 할 일 목록을 저장하기 위해 로컬 스토리지에 데이터를 저장
 function savePendingTask(task) { 
     pendingTasks.push(task); 
 } 
 
+// 완료한 항목 목록에서 특정 ID의 항목을 찾아 반환
 function findInFinished(taskId) { 
     return finishedTasks.find(function(task) { 
         return task.id === taskId;
     });
 }
 
+// 보류 중인 항목 목록에서 특정 ID의 항목을 찾아 반환
 function findInPending(taskId) { 
     return pendingTasks.find(function(task) { 
         return task.id === taskId; 
     }); 
 } 
 
+// 특정 ID의 항목을 보류 중 항목 목록에서 제거
 function removeFromPending(taskId) { 
     pendingTasks = pendingTasks.filter(function(task) { 
         return task.id !== taskId; 
     }); 
 } 
-
+// 특정 ID의 항목을 완료한 항목 목록에서 제거
 function removeFromFinished(taskId) { 
     finishedTasks = finishedTasks.filter(function(task) { 
         return task.id !== taskId; 
     });
 } 
 
+// 항목을 완료한 항목 목록에 추가
 function addToFinished(task) { 
     finishedTasks.push(task); 
 } 
 
+// 항목을 보류 중인 항목 목록에 추가
 function addToPending(task) { 
     pendingTasks.push(task);
 } 
 
+// 항목을 삭제하고, 해당 항목을 보류 중 및 완료한 항목 목록에서 모두 제거 후, 상태를 저장
 function deleteTask(e) { 
     const li = e.target.parentNode; 
     li.parentNode.removeChild(li); 
@@ -60,6 +68,7 @@ function deleteTask(e) {
     saveState(); 
 } 
 
+// 항목을 완료한 항목 목록에 이동하고, 해당 항목을 보류 중 목록에서 제거 후, 상태를 저장
 function handleFinishClick(e) { 
     const li = e.target.parentNode; 
     li.parentNode.removeChild(li); 
@@ -70,6 +79,7 @@ function handleFinishClick(e) {
     saveState(); 
 } 
 
+// 완료한 항목을 보류 중 목록에 다시 이동하고, 해당 항목을 완료한 항목 목록에서 제거한 후, 상태를 저장
 function handleBackClick(e) { 
     const li = e.target.parentNode; 
     li.parentNode.removeChild(li);
@@ -80,6 +90,7 @@ function handleBackClick(e) {
     saveState(); 
 } 
 
+// 보류 중인 항목을 화면에 표시
 function paintPendingTask(task) { 
     const genericLi = buildGenericLi(task); 
     const span = document.createElement("span"); 
@@ -91,6 +102,7 @@ function paintPendingTask(task) {
     pendingList.append(genericLi); 
 }
 
+// 항목을 보여주는 리스트 아이템을 생성
 function buildGenericLi(task) { 
     const li = document.createElement("li"); 
     const span = document.createElement("span"); 
@@ -101,8 +113,9 @@ function buildGenericLi(task) {
     li.append(span, deleteBtn);
     li.id = task.id;
     return li; 
-} 
+}
 
+// 완료한 항목을 화면에 표시
 function paintFinishedTask(task) { 
     const genericLi = buildGenericLi(task); 
     const backBtn = document.createElement("button"); 
@@ -111,20 +124,24 @@ function paintFinishedTask(task) {
     genericLi.append(backBtn); finishedList.append(genericLi); 
 } 
 
+// 현재의 보류 중 및 완료한 항목 목록을 로컬 스토리지에 저장
 function saveState() { localStorage.setItem(PENDING, JSON.stringify(pendingTasks)); 
     localStorage.setItem(FINISHED, JSON.stringify(finishedTasks)); 
 } 
 
+// 이전에 저장한 항목 목록을 로드
 function loadState() { 
     pendingTasks = JSON.parse(localStorage.getItem(PENDING)) || []; 
     finishedTasks = JSON.parse(localStorage.getItem(FINISHED)) || []; 
 } 
 
+// 이전에 저장한 항목 목록을 화면에 복원
 function restoreState() { 
     pendingTasks.forEach(function(task) { paintPendingTask(task); }); 
     finishedTasks.forEach(function(task) { paintFinishedTask(task); }); 
 } 
 
+// 사용자가 입력한 새로운 항목을 생성하여 보류 중 목록에 추가하고, 입력 필드를 지움
 function handleFormSubmit(e) {
     e.preventDefault(); 
     const taskObj = getTaskObject(input.value); 
--- src/weather.js
+++ src/weather.js
@@ -2,22 +2,25 @@ const COORDS = 'coords';
 const API_KEY = "10f39fedb504d1474f48592718b15859";
 const weather = document.querySelector(".js-weather");
 
+// 위도(lat), 경도(lng)를 사용하여 OpenWeatherMap API를 호출하여 날씨 정보를 가져오는 함수. 날씨 정보는 섭씨 온도로 출력
 function getWeather(lat,lng) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric
     `).then(function(response) {
         return response.json();
     })
     .then(function(json) {
-        const temperature = json.main.temp;
-        const place = json.name;
+        const temperature = json.main.temp; // 위도와 경도
+        const place = json.name; // 지역명
         weather.innerText = `${temperature} @ ${place}`
     });
 }
 
+// 사용자의 현재 위치 정보를 로컬 스토리지에 저장
 function saveCoords(coordsObj) {
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
 }
 
+// 브라우저의 위치 정보를 성공적으로 가져오면 호출되는 콜백 함수로, 위도와 경도를 얻어서 getWeather 함수를 호출하고 위치 정보를 저장
 function handleGeoSucces(position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
@@ -29,14 +32,17 @@ function handleGeoSucces(position) {
     getWeather(latitude, longitude);
 }
 
+// 위치 정보를 가져오지 못할 때 호출되는 콜백 함수로, 에러 메시지를 출력
 function handleGeoError() {
     console.log('Cant acces geo location')
 }
 
+// 위치 정보를 요청하기 위해 navigator.geolocation.getCurrentPosition 함수 호출
 function askForCoords() {
     navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError)
 }
 
+// 로컬 스토리지에서 저장된 위치 정보를 가져와서 날씨 정보를 얻어와 화면에 출력
 function loadCoords() {
     const loadedCoords = localStorage.getItem(COORDS);
     if(loadedCoords === null) {
@@ -47,6 +53,7 @@ function loadCoords() {
     }
 }
 
+// 스크립트를 초기화하는 함수로, 위치 정보를 로드하고 날씨 정보를 가져와 화면에 출력
 function init() {
     loadCoords();
 }
